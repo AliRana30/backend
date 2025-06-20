10a06db08f10a0743c83f66850b5dee06b599ffe
Alirana2829 <alimahmoodrana00@gmail.com>
2025-06-20 17:00:22 +0500

    add

--- controllers/signup.js
+++ controllers/signup.js
@@ -42,7 +42,7 @@ const Signup = async (req, res) => {
         res.cookie('token', authToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'None',
+            sameSite: 'Lax',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
 
