845efaf1cc774d7411a21c2d6003d012f3d9f7f2
Alirana2829 <alimahmoodrana00@gmail.com>
2025-08-30 03:37:54 +0500

    update

--- utils/mongo.js
+++ utils/mongo.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () =>{
     try{
         await mongoose.connect(process.env.MONGO_URI)
+        console.log("Mongo Connected")
     }
     catch(err){
         console.error('Error connecting to MongoDB:', err.message);
